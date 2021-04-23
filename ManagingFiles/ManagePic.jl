e1c0f2934df93c56bbb02a01df8c4675e4a6d50f
Blueberrybug <82609681+Blueberrybug@users.noreply.github.com>
2021-04-23 11:46:37 +0200

    Komentare i kosmetyka ManagePic.jl

--- ManagingFiles/ManagePic.jl
+++ ManagingFiles/ManagePic.jl
@@ -6,20 +6,21 @@ using Images, ImageShow, FileIO
 export RGB, save
 
 function RGB(filename)
-    img = load("Meg.jpg")
+    img = load(filename)
     global r = (Float64.(red.(img)))
     global g = (Float64.(green.(img)))
     global b = (Float64.(blue.(img)))
     
 end #RGB
 
-function save(filename, r, g, b)
+function save(new_filename, r, g, b)
     aa = ([RGB(r[i,j],g[i, j],b[i,j]) for i in 1:size(r)[1], j in 1:size(r)[2] ]) #z powrotem do RGB
-    save(filename,aa)
-end
+    save(new_filename, aa)
+end #save
 
 function HSL()
-end
+    #wzory needed, będzie działało jak RGB
+end #HSL
 
 
 end #ManagePic
