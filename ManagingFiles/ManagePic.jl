33d588307794f1ad614d85a7bb56004a213c8ee5
Blueberrybug <82609681+Blueberrybug@users.noreply.github.com>
2021-04-23 12:10:13 +0200

    ManagePic.jl - Naprawiłam moje głupie nadpisywanie funkcji przez które po jednym zastosowaniu nie działały

--- ManagingFiles/ManagePic.jl
+++ ManagingFiles/ManagePic.jl
@@ -1,11 +1,11 @@
 #ManagePic.jl
 module ManagePic
 
-using Images, ImageShow, FileIO 
+using FileIO, Images, ImageShow
 
 export RGB, save
 
-function RGB(filename)
+function generate_matrices_RGB(filename)
     img = load(filename)
     global r = (Float64.(red.(img)))
     global g = (Float64.(green.(img)))
@@ -13,12 +13,12 @@ function RGB(filename)
     
 end #RGB
 
-function save(new_filename, r, g, b)
+function save_pictures(new_filename, r, g, b)
     aa = ([RGB(r[i,j],g[i, j],b[i,j]) for i in 1:size(r)[1], j in 1:size(r)[2] ]) #z powrotem do RGB
     save(new_filename, aa)
 end #save
 
-function HSL()
+function generate_matrices_HSL(filename)
     #wzory needed, będzie działało jak RGB
 end #HSL
 
