1f376a6b377d9b91171b22a5d737c74699d7b318
Blueberrybug <82609681+Blueberrybug@users.noreply.github.com>
2021-04-29 21:04:58 +0200

    krótka instrukcja i sprzątanie

--- ManagingFiles/ManagePic.jl
+++ ManagingFiles/ManagePic.jl
@@ -1,31 +1,21 @@
 #ManagePic.jl
 module ManagePic
-
+"""
+modół do obsługi plików. użyjcie include() i using .ManagePic !!
+"""
 using FileIO, Images, ImageShow
 
 export generate_matrices_RGB, save_pictures
 
-<<<<<<< HEAD
 function generate_matrices_RGB(filename)
     img = load(filename)
-    global r = (Float64.(red.(img)))
-    global g = (Float64.(green.(img)))
-    global b = (Float64.(blue.(img)))
-=======
-function RGBconv(filename)
-    img = FileIO.load(filename)
     r = (Float64.(red.(img)))
     g = (Float64.(green.(img)))
     b = (Float64.(blue.(img)))
->>>>>>> 9fb52ec19a380fde8df1830c90b2bf56cca4e1f4
-    
+    return(r,g,b)
 end #RGB
 
-<<<<<<< HEAD
 function save_pictures(new_filename, r, g, b)
-=======
-function savefile(filename, r, g, b)
->>>>>>> 9fb52ec19a380fde8df1830c90b2bf56cca4e1f4
     aa = ([RGB(r[i,j],g[i, j],b[i,j]) for i in 1:size(r)[1], j in 1:size(r)[2] ]) #z powrotem do RGB
     save(new_filename, aa)
 end #save
