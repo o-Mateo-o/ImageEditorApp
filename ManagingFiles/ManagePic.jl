fd0a17961c27f7c9a83d2ad79ea758aadc0deee5
Blueberrybug <82609681+Blueberrybug@users.noreply.github.com>
2021-05-06 16:23:40 +0200

    naprawki

--- ManagingFiles/ManagePic.jl
+++ ManagingFiles/ManagePic.jl
@@ -16,6 +16,7 @@ end #RGB
 function save_pictures(new_filename, r, g, b)
     aa = ([RGB(r[i,j],g[i, j],b[i,j]) for i in 1:size(r)[1], j in 1:size(r)[2] ]) #z powrotem do RGB
     save(new_filename, aa)
+    return aa
 end #save
 
 function generate_matrices_HSL(filename)
