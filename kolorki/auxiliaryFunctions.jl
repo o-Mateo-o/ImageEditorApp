84ef78e696641b5361f61d41923215a640172f65
Jagoda Bilska <262341@student.pwr.edu.pl>
2021-05-20 18:32:07 +0200

    wyrzucanie śmieci

--- kolorki/auxiliaryFunctions.jl
+++ kolorki/auxiliaryFunctions.jl
@@ -12,53 +12,6 @@ function getValues(rgb)
     return [Cmax, middleValue, Cmin]
 end
 
-#może nazwa "setNewValues"?
-function setValues(rgb, newHighValue, newMiddleValue, newLowValue)
-    r,g,b = rgb
-    arraySize = size(r)
-    
-    newRed = zeros(Float64,arraySize[1],arraySize[2])
-    newGreen = zeros(Float64,arraySize[1],arraySize[2])
-    newBlue = zeros(Float64,arraySize[1],arraySize[2])
-    
-    Cmax = max.(r,g,b)
-    Cmin = min.(r,g,b)
-    
-    for i in 1:arraySize[1], j in 1:arraySize[2]
-        if Cmax[i,j] == r[i,j]
-            newRed[i,j] = newHighValue[i,j]
-            if Cmin[i,j] == g[i,j]
-                newGreen[i,j] = newLowValue[i,j]
-                newBlue[i,j] = newMiddleValue[i,j]
-            else
-                newBlue[i,j] = newLowValue[i,j]
-                newGreen[i,j] = newMiddleValue[i,j]    
-            end
-        
-        elseif Cmax[i,j] == g[i,j]
-            newGreen[i,j] = newHighValue[i,j]
-            if Cmin[i,j] == r[i,j]
-                newRed[i,j] = newLowValue[i,j]
-                newBlue[i,j] = newMiddleValue[i,j]
-            else
-                newBlue[i,j] = newLowValue[i,j]
-                newRed[i,j] = newMiddleValue[i,j]  
-            end
-
-        else
-            newBlue[i,j] = newHighValue[i,j]
-            if Cmin[i,j] == g[i,j]
-                newGreen[i,j] = newLowValue[i,j]
-                newRed[i,j] = newMiddleValue[i,j]
-            else
-                newRed[i,j] = newLowValue[i,j]
-                newGreen[i,j] = newMiddleValue[i,j]  
-            end
-        end 
-    end   
-    return [newRed, newGreen, newBlue]
-end
-
 
 function setValuesLighten(rgb, newHighValue, newMiddleValue, newLowValue) #wersja git do rozjaśniania i przyciemniania 
     r,g,b = rgb
