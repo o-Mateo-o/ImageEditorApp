bc11e7fe42fb30a4f51bcf84a29967901249640e
Jagoda Bilska <262341@student.pwr.edu.pl>
2021-05-27 15:02:14 +0200

    Aktualizacja funkcji HSL i L

--- ManagingFiles/ManagePic.jl
+++ ManagingFiles/ManagePic.jl
@@ -35,39 +35,42 @@ Return image as HSL matrices tuple.
 """
 function rgb2hsl(rgb)
     r, g, b = rgb
+
     arraySize = size(r)
     h = zeros(Float64,arraySize[1],arraySize[2])
     s = zeros(Float64,arraySize[1],arraySize[2])
     l = zeros(Float64,arraySize[1],arraySize[2])
 
     # L:Lightness - value of white light
-    l = (max.(r,g,b) .+ min.(r,g,b))./2
+    l = lightness(rgb)
+    
+    Cmax = max.(r,g,b)
+    Cmin = min.(r,g,b)
+    Δ = Cmax .- Cmin
+    
     
     for i in 1:arraySize[1], j in 1:arraySize[2]
-        Cmax = max(r[i,j], g[i,j], b[i,j])
-        Cmin = min(r[i,j], g[i,j], b[i,j])
-        Δ = Cmax - Cmin
     
         # H:Hue - value of color
-        if Δ == 0
+        if Δ[i,j] == 0
             h[i,j] = 0
-        elseif Cmax == r[i,j]
-            h[i,j] = round(60*(((g[i,j]-b[i,j])./Δ).%6))
-        elseif Cmax == g[i,j]
-            h[i,j] = round(60*(((g[i,j]-b[i,j])./Δ).+2))
+        elseif Cmax[i,j] == r[i,j]
+            h[i,j] = 60*(((g[i,j]-b[i,j])./Δ[i,j]).%6)
+        elseif Cmax[i,j] == g[i,j]
+            h[i,j] = 60*(((b[i,j]-r[i,j])./Δ[i,j]).+2)
         else
-            h[i,j] = round(60*(((r[i,j]-g[i,j])./Δ).+4))
+            h[i,j] = 60*(((r[i,j]-g[i,j])./Δ[i,j]).+4)
         end
         
         #S:Saturation -  color saturation 
-        if Δ == 0
+        if Δ[i,j] == 0
             s[i,j] = 0
         else
-            s[i,j] = round(Δ./(1-abs(2*lum-1)), digits = 3)
+            s[i,j] = Δ[i,j]./(1-abs(2*l[i,j]-1))
         end
     end
     return [h,s,l]
-end #HSL
+end
 
 """
 Convert HSL to RGB.
@@ -75,7 +78,6 @@ Return image as RGB matrices tuple.
 :param hsl::Array{Array{Float64,2},1}; HSL matrices tuple
 """
 function hsl2rgb(hsl)
-
     h, s, l = hsl
     arraySize = size(h)
     r = zeros(Float64,arraySize[1],arraySize[2])
@@ -83,10 +85,10 @@ function hsl2rgb(hsl)
     b = zeros(Float64,arraySize[1],arraySize[2])
     
     for i in 1:arraySize[1], j in 1:arraySize[2]
-        C = (1-abs(2l[i,j]-1)).*s[i,j]
-        X = round(C.*(1-abs((h[i,j]./60)%2 - 1)), digits = 3)
-        m = round(l[i,j] - C./2, digits = 3)
-            
+        C = (1-abs(2*l[i,j]-1)).*s[i,j]
+        X = C.*(1-abs((h[i,j]./60)%2 - 1))
+        m = l[i,j] - C./2
+
         if h[i,j]>=0 && h[i,j]<60
             r[i,j] = C+m
             g[i,j] = X+m
@@ -105,7 +107,7 @@ function hsl2rgb(hsl)
             b[i,j] = C+m
         elseif h[i,j]>=240 && h[i,j]<300
             r[i,j] = X+m
-            g[i,j] = 0+m
+            g[i,j] = m
             b[i,j] = C+m
         else
             r[i,j] = C+m
@@ -115,6 +117,7 @@ function hsl2rgb(hsl)
     end
     return [r,g,b]
 end 
+
 
 """
 Count lightness.
